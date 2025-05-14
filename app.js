ed6b3abe03566ee2b3789ddf6c23d9b042caf1c1
Aline Anjos <lianjos@hotmail.com.br>
2025-05-14 12:16:29 -0300

    Alterando limite para 50

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function verificarChute() {
 //Obs.: No JS é possível inverter a ordem (função criada e função chamada). É possível colocar a função chamada antes da função criada e mesmo assim o JS funciona.
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
