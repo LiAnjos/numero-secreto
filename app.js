b531f00a005cc1071e0241bc2db1750181ea38da
Aline Anjos <lianjos@hotmail.com.br>
2025-05-14 08:09:35 -0300

    Alterando limite para 100

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela("h1", "Jogo do Número Secreto");
-    exibirTextoNaTela("p", "Escolha um número de 1 a 10");
+    exibirTextoNaTela("p", "Escolha um número de 1 a 100");
 }
 
 exibirMensagemInicial();
